de5c37a24bb87de1142be749e0db7279234059be
Gitea <gitea@fake.local>
2020-03-31 18:16:01 -0400

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.7-alpine
+
+ENV BASE_URL "unix:///var/run/docker.sock"
+ENV ARKCMD = ""
+ENV THEME = "darkly"
+ENV HOST = "0.0.0.0"
+ENV PORT = "8888"
+
+WORKDIR /usr/src/app
+COPY requirements.txt ./
+COPY server.py ./
+COPY templates ./templates/
+
+RUN pip install -r requirements.txt
+
+VOLUME /var/run/docker.sock
+EXPOSE $PORT
+
+CMD ["python", "./server.py"]
---
+++ server.py
@@ -0,0 +1,51 @@
+"""
+Simple ARK: Survival Evolved CLuster Status Page
+"""
+
+import os
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from docker.client import APIClient
+
+BASE_URL = os.getenv("DOCKER_URL", "unix:///var/run/docker.sock")
+ARKCMD = os.getenv("ARKCMD", "echo $HOSTNAME")
+THEME = os.getenv("THEME", "darkly")
+
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = int(os.getenv("PORT", "8888"))
+
+APP = Flask(__name__)
+Bootstrap(APP)
+
+DOCKER = APIClient(base_url=BASE_URL)
+
+
+@APP.route("/")
+def index():
+    """
+    Creates status page.
+    :return:
+    """
+    containers = []
+    for container in DOCKER.containers(all=True):
+        if str(container['Status']).startswith("Up"):
+            cmd = DOCKER.exec_create(container['Id'], ARKCMD)
+            containers.append({
+                "Id": container['Id'],
+                "Name": container['Names'][0],
+                "Status": container['Status'],
+                "Command": DOCKER.exec_start(cmd['Id']).decode("utf-8"),
+                "Color": ""
+            })
+        else:
+            containers.append({
+                "Id": container['Id'],
+                "Name": container['Names'][0],
+                "Status": container['Status'],
+                "Command": "",
+                "Color": "danger"
+            })
+    return render_template("index.html", containers=containers, theme=THEME)
+
+
+APP.run(HOST, PORT)
