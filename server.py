9e116f4dd8e4a676e7fd8d3994876b1364f49ebe
Gitea <gitea@fake.local>
2020-04-01 15:30:49 -0400

    Added printable filter to output.

--- server.py
+++ server.py
@@ -36,11 +36,12 @@ def index():
     for container in [x for x in DOCKER.containers(all=True) if re.match(CFILTER, x['Names'][0].lstrip("/"))]:
         if str(container['Status']).startswith("Up"):
             cmd = DOCKER.exec_create(container['Id'], ARKCMD)
+            cmd = ''.join(filter(lambda x: x in set(string.printable), DOCKER.exec_start(cmd['Id']).decode("utf-8")))
             containers.append({
                 "Id": container['Id'],
                 "Name": container['Names'][0],
                 "Status": container['Status'],
-                "Command": filter(lambda x: x in set(string.printable), DOCKER.exec_start(cmd['Id']).decode("utf-8")),
+                "Command": cmd,
                 "Color": ""
             })
         else:
